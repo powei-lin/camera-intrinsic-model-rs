77f298f84f0782d9a0dd1fddb84e8ef023190d5d
Powei Lin <poweilin1994@gmail.com>
2024-12-01 20:21:22 -0500

    ftheta

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,11 +1,13 @@
 pub mod eucm;
 pub mod eucmt;
+pub mod ftheta;
 pub mod kb4;
 pub mod opencv5;
 pub mod ucm;
 
 pub use eucm::*;
 pub use eucmt::*;
+pub use ftheta::*;
 pub use kb4::*;
 pub use opencv5::*;
 pub use ucm::*;
