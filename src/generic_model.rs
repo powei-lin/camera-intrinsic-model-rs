c45a8898d486c883b0ffa65d06b199c0b0e04870
Powei Lin <poweilin1994@gmail.com>
2024-12-01 22:10:22 -0500

    rename

--- src/generic_model.rs
+++ src/generic_model.rs
@@ -1,5 +1,6 @@
 use std::str::FromStr;
 
+use super::generic_functions::*;
 use super::{Ftheta, KannalaBrandt4, OpenCVModel5, EUCM, EUCMT, UCM};
 use image::DynamicImage;
 use nalgebra as na;
@@ -228,95 +229,4 @@ where
             })
             .collect()
     }
-}
-
-pub fn init_undistort_map(
-    camera_model: &dyn CameraModel<f64>,
-    projection_mat: &na::Matrix3<f64>,
-    new_w_h: (u32, u32),
-) -> (na::DMatrix<f32>, na::DMatrix<f32>) {
-    if projection_mat.shape() != (3, 3) {
-        panic!("projection matrix has the wrong shape");
-    }
-    let fx = projection_mat[(0, 0)];
-    let fy = projection_mat[(1, 1)];
-    let cx = projection_mat[(0, 2)];
-    let cy = projection_mat[(1, 2)];
-    let p3ds: Vec<na::Vector3<f64>> = (0..new_w_h.1)
-        .into_par_iter()
-        .flat_map(|y| {
-            (0..new_w_h.0)
-                .into_par_iter()
-                .map(|x| na::Vector3::new((x as f64 - cx) / fx, (y as f64 - cy) / fy, 1.0))
-                .collect::<Vec<na::Vector3<f64>>>()
-        })
-        .collect();
-    let p2ds = camera_model.project(&p3ds);
-    let (xvec, yvec): (Vec<f32>, Vec<f32>) = p2ds
-        .par_iter()
-        .map(|xy| {
-            if let Some(xy) = xy {
-                (xy[0] as f32, xy[1] as f32)
-            } else {
-                (f32::NAN, f32::NAN)
-            }
-        })
-        .unzip();
-    let xmap = na::DMatrix::from_vec(new_w_h.1 as usize, new_w_h.0 as usize, xvec);
-    let ymap = na::DMatrix::from_vec(new_w_h.1 as usize, new_w_h.0 as usize, yvec);
-    (xmap, ymap)
-}
-
-pub fn estimate_new_camera_matrix_for_undistort(
-    camera_model: &dyn CameraModel<f64>,
-    balance: f64,
-    new_image_w_h: Option<(u32, u32)>,
-) -> na::Matrix3<f64> {
-    if !(0.0..=1.0).contains(&balance) {
-        panic!("balance should be [0.0-1.0], got {}", balance);
-    }
-    let params = camera_model.params();
-    let cx = params[2];
-    let cy = params[3];
-    let w = camera_model.width();
-    let h = camera_model.height();
-    let p2ds = vec![
-        na::Vector2::new(cx, 0.0),
-        na::Vector2::new(w - 1.0, cy),
-        na::Vector2::new(cx, h - 1.0),
-        na::Vector2::new(0.0, cy),
-    ];
-    let undist_pts = camera_model.unproject(&p2ds);
-    let mut min_x = f64::MAX;
-    let mut min_y = f64::MAX;
-    let mut max_x = f64::MIN;
-    let mut max_y = f64::MIN;
-    for p in undist_pts {
-        let p = p.unwrap();
-        min_x = min_x.min(p.x);
-        min_y = min_y.min(p.y);
-        max_x = max_x.max(p.x);
-        max_y = max_y.max(p.y);
-    }
-    min_x = min_x.abs();
-    min_y = min_y.abs();
-    let (new_w, new_h) = if let Some((new_w, new_h)) = new_image_w_h {
-        (new_w, new_h)
-    } else {
-        (camera_model.width() as u32, camera_model.height() as u32)
-    };
-    let new_cx = new_w as f64 * min_x / (min_x + max_x);
-    let new_cy = new_h as f64 * min_y / (min_y + max_y);
-    let fx = new_w as f64 / (min_x + max_x);
-    let fy = new_h as f64 / (min_y + max_y);
-    let fmin = fx.min(fy);
-    let fmax = fx.max(fy);
-    let new_f = balance * fmin + (1.0 - balance) * fmax;
-
-    let mut out = na::Matrix3::identity();
-    out[(0, 0)] = new_f;
-    out[(1, 1)] = new_f;
-    out[(0, 2)] = new_cx;
-    out[(1, 2)] = new_cy;
-    out
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
-pub mod generic;
+pub mod generic_functions;
+pub mod generic_model;
 pub mod io;
 pub mod models;
 
-pub use generic::*;
+pub use generic_functions::*;
+pub use generic_model::*;
 pub use io::*;
 pub use models::*;
