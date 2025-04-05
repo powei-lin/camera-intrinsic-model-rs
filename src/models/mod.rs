e27657aace1860f07d4f6b0496dd9297840f6ecc
Powei Lin <poweilin1994@gmail.com>
2025-04-05 13:56:58 -0400

    add fov (#4)

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,5 +1,6 @@
 pub mod eucm;
 pub mod eucmt;
+pub mod fov_camera;
 pub mod ftheta;
 pub mod kb4;
 pub mod opencv5;
@@ -7,6 +8,7 @@ pub mod ucm;
 
 pub use eucm::*;
 pub use eucmt::*;
+pub use fov_camera::*;
 pub use ftheta::*;
 pub use kb4::*;
 pub use opencv5::*;
