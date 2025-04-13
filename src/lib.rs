9b74e882c95a4900b71a896f0ff2422215a2267c
Powei Lin <poweilin1994@gmail.com>
2025-04-13 14:25:20 -0400

    add doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+//! camera-intrinsic-model is a library for distort/undistort images.
+//! # Examples
+//!
+//! ```
+//! use camera_intrinsic_model::*;
+//! let model = model_from_json("data/eucm0.json");
+//! let new_w_h = 1024;
+//! let p = model.estimate_new_camera_matrix_for_undistort(0.0, Some((new_w_h, new_w_h)));
+//! let (xmap, ymap) = model.init_undistort_map(&p, (new_w_h, new_w_h), None);
+//! // let remaped = remap(&img, &xmap, &ymap);
+//! ```
 pub mod generic_functions;
 pub mod generic_model;
 pub mod io;
